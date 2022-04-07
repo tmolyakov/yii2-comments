932d894564435673f54e2ff0bd07e41f0cf2ab0f
Anatoliy <tmolyakov@gmail.com>
2022-04-07 21:17:37 +0300

    smth

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=mysql;dbname=yii2_mysql',
     'username' => 'root',
-    'password' => '',
+    'password' => 'rootexample',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
